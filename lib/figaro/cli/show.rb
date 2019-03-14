4b1a1e1cf8ced1e73377992e3e6f7aaab472aee7
Lucas Babey <babeylucas@gmail.com>
2019-03-14 11:49:50 +0100

    Show vars with ENV format

--- lib/figaro/cli/show.rb
+++ lib/figaro/cli/show.rb
@@ -7,7 +7,7 @@ module Figaro
         longest_key = configuration.keys.max { |key| key.length }
         padding = (longest_key.length + 3)
         configuration.sort_by { |k, v| k }.each do |k, v|
-          puts "%- #{padding}s: %s" % [k, v]
+          puts "%- #{padding}s=%s" % [k, v]
         end
       end
     end
