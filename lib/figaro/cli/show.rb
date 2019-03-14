659a4aa9f1219d3e720c7c70deb7c7f7df7896b1
Lucas Babey <babeylucas@gmail.com>
2019-03-14 11:51:33 +0100

    [Env] Remove padding for ENV Format

--- lib/figaro/cli/show.rb
+++ lib/figaro/cli/show.rb
@@ -4,10 +4,8 @@ module Figaro
   class CLI < Thor
     class Show < Task
       def run
-        longest_key = configuration.keys.max { |key| key.length }
-        padding = (longest_key.length + 3)
         configuration.sort_by { |k, v| k }.each do |k, v|
-          puts "%- #{padding}s=%s" % [k, v]
+          puts "%s=%s" % [k, v]
         end
       end
     end
